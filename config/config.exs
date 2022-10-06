e62b9b334a9120499979596e66cd249b882e08b8
Brian Hall <brian.hall@nfiindustries.com>
2022-10-06 13:16:10 -0500

    Revert "Recreated book Music_DB for understanding"
    This reverts commit 93f3116fcc0fe0dfd617b4c1d390ea6b85fed408.

--- config/config.exs
+++
@@ -1,9 +0,0 @@
-import Config
-
-config :music_db_new,
-  ecto_repos: [MusicDBNew.Repo]
-config :music_db_new, MusicDBNew.Repo,
-  database: "music_db_new_repo",
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost"
--- lib/music_db_new/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule MusicDBNew.Repo do
-  use Ecto.Repo,
-    otp_app: :music_db_new,
-    adapter: Ecto.Adapters.Postgres
-end
